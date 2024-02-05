9bd4518e2d2367db487691767dc60895a4dc6702
arthur972645 <arthurall.1973@gmail.com>
2024-02-05 10:52:09 -0300

    objeto

--- Objeto/objeto.js
+++ Objeto/objeto.js
@@ -2,8 +2,21 @@ let pessoa = {
     nome: 'Arthur',
     idade: 17,
     altura: 1.84,
+    endereco: [ //LEMBRE-SE array é [], Objeto é {}, entao endereco é cum array
+        {
+            tipo: 'Residencial',
+            rua: 'Rua A',
+            Cidade: 'Maceio',
+        },
+        {
+            tipo: 'Residencial',
+            rua: 'Rua B',
+            Cidade: 'Salvador',
+        },
+    ],
     saudacao(){
         return 'Ol, mundo'
     }
 }
-console.log(pessoa.nome)
+const endereco = (elementos) => console.log(elementos.tipo)
+pessoa.endereco.map(endereco) // pessoa. endereco é tipo vc chamando o array 
